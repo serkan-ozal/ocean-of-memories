4950a12e4d02781ebf2fec96f33c48d7f6d74e31
unknown <Asus@Asus-Bilgisayar.(none)>
2013-04-07 20:25:55 +0300

    MemoryUtil and Util classes added for common use in all articles

---
+++ src/main/java/tr/com/javalanche/oceanofmemories/common/util/MemoryUtil.java
@@ -0,0 +1,86 @@
+package tr.com.javalanche.oceanofmemories.common.util;
+
+import java.lang.reflect.Field;
+
+import sun.misc.Unsafe;
+
+public class MemoryUtil {
+
+	public static final long BYTE_COUNT_IN_KB = 1024;
+	public static final long BYTE_COUNT_IN_MB = 1024 * BYTE_COUNT_IN_KB;
+	public static final long KB_COUNT_IN_MB = 1024;
+	
+	public static final int NR_BITS = Integer.valueOf(System.getProperty("sun.arch.data.model"));
+	public static final int BYTE = 8;
+	public static final int WORD = NR_BITS / BYTE;
+	
+	private static Unsafe unsafe;
+	
+	static {
+		init();
+	}
+	
+	private MemoryUtil() {
+		
+	}
+	
+	private static void init() {
+		try {
+            Field field = Unsafe.class.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            unsafe = (Unsafe) field.get(null);
+        }
+        catch (Exception e) {
+        	e.printStackTrace();
+        }
+	}
+	
+	public static void info() {
+        System.out.println("Unsafe: " + unsafe);
+        System.out.println("\tAddressSize : " + unsafe.addressSize());
+        System.out.println("\tPage Size   : " + unsafe.pageSize());
+    }
+	
+	public static double byte2KB(long sizeAsBytes) {
+		return Util.makeDouble2DigitPrecisioned((double)sizeAsBytes / BYTE_COUNT_IN_KB);
+	}
+	
+	public static double byte2MB(long sizeAsBytes) {
+		return Util.makeDouble2DigitPrecisioned((double)sizeAsBytes / BYTE_COUNT_IN_MB);
+	}
+	
+	public static double kb2MB(long sizeAsKBs) {
+		return Util.makeDouble2DigitPrecisioned((double)sizeAsKBs / KB_COUNT_IN_MB);
+	}
+	
+	public static void runGC() {
+		long start = System.nanoTime();
+		for (int i = 0; i < 3; i++) {
+			System.gc();
+		}
+		long finish = System.nanoTime();
+		System.out.println("GC executed in " + ((finish - start) / 1000000) + " milliseconds ...\n");
+	}
+	
+	public static void printMemoryUsage() {
+		Runtime runtime = Runtime.getRuntime();
+		System.out.println(String.format("%-10s %-10s %-10s", "MAX", "TOTAL", "FREE"));
+		System.out.println("========== ========== ==========");
+		System.out.println(String.format("%10d %10d %10d\n\n", runtime.maxMemory(), runtime.totalMemory(), runtime.freeMemory()));
+	}
+	
+	public static void dumpFromObject(Object obj, long size) {
+		int sizeLength = String.valueOf(size).length();
+    	for (int i = 0; i < size; i++) {
+    		if (i % 16 == 0) {
+    			System.out.print(String.format("[%0" + sizeLength + "d]: ", i));
+    		}
+	    	System.out.print(String.format("%02x ", unsafe.getByte(obj, (long)i)));
+			if ((i + 1) % 16 == 0) {
+				System.out.println();
+			}
+    	}	
+    	System.out.println();
+    }
+
+}
---
+++ src/main/java/tr/com/javalanche/oceanofmemories/common/util/Util.java
@@ -0,0 +1,79 @@
+package tr.com.javalanche.oceanofmemories.common.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URLDecoder;
+
+public class Util {
+	
+	private Util() {
+		
+	}
+	
+	public static double makeDouble2DigitPrecisioned(double value) {
+		return ((int)(value * 100) / (double)100);
+	}
+	
+	public static double percantegeOf(double value1, double value2) {
+		return makeDouble2DigitPrecisioned((value1 * 100) / value2);
+	}
+	
+	public static void waitFor(long millisecods) {
+		try {
+			Thread.sleep(millisecods);
+		} 
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	public static void runClass(Class<?> classHasMainMethod, String[] vmArguments, String[] programArguments) {
+		try {
+			StringBuilder sbVmArguments = new StringBuilder();
+			StringBuilder sbProgramArguments = new StringBuilder();
+			
+			if (vmArguments != null) {
+				for (String arg : vmArguments) {
+					sbVmArguments.append(arg).append(" ");
+				}
+			}
+			
+			if (programArguments != null) {
+				for (String arg : programArguments) {
+					sbProgramArguments.append(arg).append(" ");
+				}
+			}
+			
+			String classPath = URLDecoder.decode(Util.class.getClassLoader().getResource(".").getPath());
+			Process proc = 
+					Runtime.getRuntime().exec(
+							"java" + " -cp " + "\"" + classPath + "\"" + " " +
+							sbVmArguments.toString() + " " +
+							classHasMainMethod.getName() + " " +
+							sbProgramArguments.toString());
+	        
+			BufferedReader outputReader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+	        BufferedReader errorReader = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+	        String line = null;
+	
+	        while ((line = outputReader.readLine()) != null) {	
+	        	System.out.println(line);
+	        }	
+	        
+	        while ((line = errorReader.readLine()) != null) {	
+	        	System.out.println(line);
+	        }
+        
+	        proc.waitFor();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		} 
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
