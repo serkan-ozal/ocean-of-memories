776db1249f2b54ddeb65f3b4c2bdf3d9de36a27c
unknown <Asus@Asus-Bilgisayar.(none)>
2013-06-12 20:21:46 +0300

    article1 about class&object layout created

---
+++ src/main/java/tr/com/javalanche/oceanofmemories/article1/classlayout/ClassLayoutDemo.java
@@ -0,0 +1,9 @@
+package tr.com.javalanche.oceanofmemories.article1.classlayout;
+
+public class ClassLayoutDemo {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/main/java/tr/com/javalanche/oceanofmemories/article1/objectlayout/ObjectLayoutDemo.java
@@ -0,0 +1,9 @@
+package tr.com.javalanche.oceanofmemories.article1.objectlayout;
+
+public class ObjectLayoutDemo {
+
+	public static void main(String[] args) {
+
+	}
+
+}
